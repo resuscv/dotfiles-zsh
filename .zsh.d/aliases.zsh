dfd3b04825def7163617f8210b97beba87a2f18b
Olaf TNSB <still.another.person@gmail.com>
2018-06-14 12:13:35 +1000

    Added colour aliases

--- .zsh.d/aliases.zsh
+++ .zsh.d/aliases.zsh
@@ -2,6 +2,17 @@
 #
 # Aliases for zsh
 
+export NONE='\033[00m'
+export RED='\033[01;31m'
+export GREEN='\033[01;32m'
+export YELLOW='\033[01;33m'
+export PURPLE='\033[01;35m'
+export CYAN='\033[01;36m'
+export WHITE='\033[01;37m'
+export BOLD='\033[1m'
+export UNDERLINE='\033[4m'
+
+
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
